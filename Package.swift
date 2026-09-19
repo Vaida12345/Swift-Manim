da95e203a5961d9b376b39938dccb4f445160096
Vaida <developer@vaidaf.net>
2026-09-19 16:53:54 +1000

    bump depedencies

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.2
+// swift-tools-version: 6.4
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -12,7 +12,7 @@ let package = Package(
         .library(name: "Manim", targets: ["Manim"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Vaida12345/FinderItem.git", from: "2.1.0"),
+        .package(url: "https://github.com/Vaida12345/FinderItem.git", from: "2.2.1"),
         .package(url: "https://github.com/pvieito/PythonKit.git", branch: "master")
     ],
     targets: [
